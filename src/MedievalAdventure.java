bbf78a387c99ef041b109a6789fa7178f481caaa
killiam-cs <charlotte_killiam@milton.edu>
2025-01-08 13:13:43 -0500

    Initial commit

---
+++ src/MedievalAdventure.java
@@ -0,0 +1,5 @@
+public class MedievalAdventure {
+
+    private String ancientRunes = "z%nxt#%lh#@wsife#w#qr$mt*h!lho#gr&tw#sxrk^o#wl#tvqu!mf#*pzl#i#e!ksv#fe*$sioe#s#i#pmn#?*wt#bh#rzfhwie#xf#jntxo#h%nr#fue#*!orzs#jst#^*@";
+
+}
