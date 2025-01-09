b8189f9ddbd3b457e3eb820c3106d91fb136862b
killiam-cs <charlotte_killiam@milton.edu>
2025-01-09 12:24:52 -0500

    Initial commit

--- src/CastleExplorer.java
+++ src/CastleExplorer.java
@@ -1,4 +1,4 @@
-public class MedievalAdventure {
+public class CastleExplorer {
 
     private String ancientRunes = "z%nxt#%lh#@wsife#w#qr$mt*h!lho#gr&tw#sxrk^o#wl#tvqu!mf#*pzl#i#e!ksv#fe*$sioe#s#i#pmn#?*wt#bh#rzfhwie#xf#jntxo#h%nr#fue#*!orzs#jst#^*@";
 
